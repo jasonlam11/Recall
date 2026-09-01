2e06a5bfb8f97e7d96e988439ffad2cbbfe916e8
Jason Lam <jasonlam276@gmail.com>
2026-09-01 15:33:27 -0400

    Initial Commit

---
+++ Recall/RecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallApp.swift
+//  Recall
+//
+//  Created by Jason Lam on 9/1/26.
+//
+
+import SwiftUI
+
+@main
+struct RecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
